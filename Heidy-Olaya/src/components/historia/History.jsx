6031efad4951828273d88c33acb4b6c0c158b288
hei-ola <lorentsvan@outlook.com>
2022-04-02 07:47:27 -0500

    clase-react

---
+++ Heidy-Olaya/src/components/historia/History.jsx
@@ -0,0 +1,96 @@
+import { Component } from 'react';
+import data from '../data.json';
+
+export default class History extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numHistory: "1",
+      cont: 0,
+      guardarDato: "",
+      historialArray: [],
+    };
+  };
+
+  componentDidUpdate() {
+    console.log("se actualizo componentDidUpdate");
+  };
+
+  handleClick = (e) => {
+    const id = e.target.id;
+    let contador = this.state.cont;
+    let numLetra = 1;
+    numLetra = numLetra + 1;
+    this.state.historialArray.push(id);
+
+    if (contador >= 7) {
+      alert("esto ya termino");
+      this.setState({
+        numHistory: "1",
+        cont: 0,
+        guardarDato: "",
+        historialArray: [],
+      });
+    } else if (contador === 0) {
+      id === "a" ? (contador = contador + 1) : (contador = contador + 2);
+
+      //cambio el state por los nuevos valores
+      this.setState({
+        numHistory: numLetra + id,
+        cont: contador,
+        guardarDato: id,
+      });
+    } else if (this.state.numHistory == numLetra + "a") {
+      id === "a" ? (contador = contador + 2) : (contador = contador + 3);
+
+      //cambio el state por los nuevos valores
+      this.setState({
+        numHistory: numLetra + id,
+        cont: contador,
+        guardarDato: id,
+      });
+    } else {
+      id === "a" ? (contador = contador + 1) : (contador = contador + 2);
+
+      //cambio el state por los nuevos valores
+      this.setState({
+        numHistory: numLetra + id,
+        cont: contador,
+        guardarDato: id,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="layout">
+        <h1 className="historia">{data[this.state.cont].historia}</h1>
+        <div className="opciones">
+          <div className="opcion">
+            <button id="a" className="botones" onClick={this.handleClick}>
+              A
+            </button>
+            <h2>{data[this.state.cont].opciones.a}</h2>
+          </div>
+          <div className="opcion">
+            <button id="b" className="botones" onClick={this.handleClick}>
+              B
+            </button>
+            <h2>{data[this.state.cont].opciones.b}</h2>
+          </div>
+        </div>
+        <div className="recordatorio">
+          <h3>
+            Seleccionado anteriormente: {this.state.guardarDato.toUpperCase()}
+          </h3>
+          <h4>Historial de opciones elegidas</h4>
+          <ul>
+            {this.state.historialArray.map((h, idx) => (
+              <li key={idx}>{h.toUpperCase()}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  };
+};
